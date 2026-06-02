bfe21807dbb71ac49f61ee13ba9971e23bb16c85
dineshparthipan27-dot <dineshparthipan27@gmail.com>
2026-06-02 10:02:43 +0530

    grocery

--- script.js
+++ script.js
@@ -48,3 +48,36 @@ prevBtn.onclick = () => {
   });
 
 };
+
+function validateEmail() {
+
+  const email = document.getElementById("subscribeEmail").value.trim();
+
+  const message = document.getElementById("emailMessage");
+
+  const emailPattern =
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (email === "") {
+
+    message.innerHTML = "Please enter your email address.";
+    message.style.color = "red";
+
+    return false;
+  }
+
+  if (!emailPattern.test(email)) {
+
+    message.innerHTML = "Please enter a valid email address.";
+    message.style.color = "red";
+
+    return false;
+  }
+  window.location.href='404.html'
+  message.innerHTML = "Subscribed successfully!";
+  message.style.color = "lime";
+
+  document.getElementById("subscribeEmail").value = "";
+
+  return true;
+}
